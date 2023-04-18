223630eb07e004281dc9a2e8f4c1549a0f5ccc04
João D <joao.luis.domingos@gmail.com>
2023-04-18 16:13:27 +0100

    Improve script

--- recipes/ethers.ts
+++ recipes/ethers.ts
@@ -1,14 +1,14 @@
-import { Wallet } from "ethers";
 import * as dotenv from "dotenv";
+import { Wallet } from "ethers";
+
 import "cross-fetch/polyfill";
-import cli from "cli-ux";
+import Enquirer from 'enquirer';
 
 dotenv.config();
 
 const ENDPOINT = process.env.API_ENDPOINT;
-const INTEGRATION_ID = "ethereum-matic-native-staking";
 
-const post = async (path: string, data: object) =>
+export const post = async (path: string, data: object) =>
   fetch(`${ENDPOINT}${path}`, {
     method: "POST",
     headers: {
@@ -18,7 +18,17 @@ const post = async (path: string, data: object) =>
     body: JSON.stringify(data),
   }).then((res) => res.json());
 
-const get = async (path: string) =>
+export const patch = async (path: string, data: object) =>
+  fetch(`${ENDPOINT}${path}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": process.env.API_KEY,
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
+
+export const get = async (path: string) =>
   fetch(`${ENDPOINT}${path}`, {
     method: "GET",
     headers: {
@@ -28,123 +38,153 @@ const get = async (path: string) =>
   }).then((res) => res.json());
 
 async function main() {
-  const wallet = await Wallet.fromPhrase(process.env.MNEMONIC);
-  const address = await wallet.getAddress();
+  try {
+    let additionalAddresses = {};
+    const wallet = Wallet.fromPhrase(process.env.MNEMONIC);
+    const address = await wallet.getAddress();
 
-  const config = await get(`/yields/${INTEGRATION_ID}`);
+    const integrations = await get(`/v1/stake/opportunities`);
 
-  console.log("=== Configuration === ");
-  console.log("ID:", config.id);
-  console.log(`APY: ${(config.apy * 100).toFixed(2)}%`);
-  console.log(`Token: ${config.token.symbol} on ${config.token.network}`);
-  console.log("=== Configuration end === ");
-
-  const [validators, gas, balance] = await Promise.all([
-    get(`/yields/validators/${INTEGRATION_ID}`),
-    get(`/network/gas/${config.token.network}`),
-    post(`/chains/token_balances`, {
-      addresses: [
-        {
-          network: config.token.network,
-          address,
-          tokenAddress: config.token.address,
-        },
-      ],
-    }),
-  ]);
-
-  const stakedBalance = await post(`/yields/balances/${INTEGRATION_ID}`, {
-    address,
-    options: { validatorAddresses: validators.map((x) => x.address) },
-  });
-
-  console.log("=== Balances ===");
-
-  console.log("Available", config.token.symbol, balance[0].amount);
-  console.log("Staked");
-  validators.forEach((x) => {
-    const balance = stakedBalance.find((y) => y.validatorAddress === x.address);
-    console.log(" ", x.name, balance.amount);
-  });
-  console.log("=== Balances end ===");
-
-  validators.forEach((x, index) => {
-    console.log(`(${index}) ${x.name}`);
-  });
-
-  const index = await cli.prompt(
-    "Which validator would you like to stake with?"
-  );
-
-  const validator = validators[index];
-  if (!validator) {
-    console.log("Invalid validator");
-    return;
-  }
-
-  const amount = await cli.prompt("How much would you like to stake?");
-
-  let gasPriceStep = {};
-  [...gas.modes.values, { name: "custom" }].forEach((g, i) => {
-    if (g.name === "custom") {
-      console.log(`(${i}) ${g.name}`);
-    } else {
-      console.log(`(${i}) ${g.name} (${g.value} ${gas.modes.denom})`);
-    }
-  });
-  const gasModeIndex = await cli.prompt(
-    `Which gas mode would you like to execute with (${gas.modes.denom})?`
-  );
-  const gasMode = gas.modes.values[gasModeIndex];
-
-  if (gasMode.name === "custom") {
-    const opts = { gasMode: gasMode.name, gasArgs: {} };
-    for (let i = 0; i < gas.suggestedValues.length; i++) {
-      const { name, recommendValue, units } = gas.suggestedValues[i];
-      const input = await cli.prompt(`Input ${name} (${units})`, {
-        default: recommendValue,
-      });
-      opts.gasArgs[name] = input;
-    }
-    gasPriceStep = opts;
-  } else {
-    gasPriceStep = { gasMode: gasMode.name };
-  }
-
-  const enter = await post("/yields/enter", {
-    integrationId: INTEGRATION_ID,
-    addresses: {
-      address: await wallet.getAddress(),
-    },
-    arguments: {
-      amount,
-      validatorAddress: validator.address,
-    },
-    gasPriceStep,
-  });
-
-  console.log(enter.txs.length, "transactions to sign");
-
-  let lastTx = null;
-  for (const { tx, network } of enter.txs) {
-    const signed = await wallet.signTransaction(JSON.parse(tx));
-    const result = await post(`/chains/submit`, {
-      network,
-      signed,
+    const { integrationId }: any = await Enquirer.prompt({
+      type: 'select',
+      name: 'integrationId',
+      message: 'Choose the integration ID you would like to test: ',
+      choices: integrations.map((integration: { id: any; }) => integration.id)
     });
-    lastTx = result;
-  }
-  while (true) {
-    const result = await get(
-      `/transaction/status/${config.token.network}/${lastTx.transactionHash}`
-    );
-    if (result.status === "success") {
-      console.log(lastTx.link);
-      break;
+
+    const config = await get(`/v1/stake/opportunities/${integrationId}`);
+
+    console.log(config);
+    console.log("=== Configuration === ");
+    console.log("ID:", config.id);
+    console.log(`APY: ${((config.apy || 1) * 100).toFixed(2)}%`);
+    console.log(`Token: ${config.token.symbol} on ${config.token.network}`);
+    console.log("=== Configuration end === ");
+
+    const [balance] = await Promise.all([
+      post(`/v1/token/balances`, {
+        addresses: [
+          {
+            network: config.token.network,
+            address,
+            tokenAddress: config.token.address,
+          },
+        ],
+      }),
+    ]);
+
+    const stakedBalance = await post(`/v1/stake/balances/${integrationId}`, {
+      address,
+    });
+
+    console.log("=== Balances ===");
+
+    console.log("Available", config.token.symbol, balance[0].amount);
+    console.log("Staked", stakedBalance);
+
+    console.log("=== Balances end ===");
+
+    const { amount }: any = await Enquirer.prompt({
+      type: 'input',
+      name: 'amount',
+      message: 'How much would you like to stake?'
+    });
+
+    console.log(address);
+    const enter = await post("/v2/stake/enter", {
+      integrationId: integrationId,
+      addresses: {
+        address: address,
+        additionalAddresses: additionalAddresses,
+      },
+      arguments: {
+        amount: amount,
+      },
+    });
+
+    console.log(enter);
+
+    let lastTx = null;
+    for (const partialTx of enter.transactions) {
+      if (partialTx.status === 'SKIPPED') {
+        continue;
+      }
+      console.log(
+        `Action ${++partialTx.stepIndex} out of ${enter.transactions.length} ${
+          partialTx.type
+        }`
+      );
+
+      const gas = await get(`/v1/transaction/gas/${config.token.network}`);
+      console.log(gas);
+
+      let gasPriceStep = {};
+      const { gasMode }: any = await Enquirer.prompt({
+        type: 'select',
+        name: 'gasMode',
+        message: `Which gas mode would you like to execute with (${gas.modes.denom})?`,
+        choices: [...gas.modes.values, { name: "custom" }].map((g) => {
+          return {message: g.name, name: {...g}};
+        })
+      });
+
+      if (gasMode.name === "custom") {
+        console.log('Custom gas mode not supported for now.');
+        throw null;
+        // const opts = { gasMode: gasMode.name, gasArgs: {} };
+        // for (let i = 0; i < gas.suggestedValues.length; i++) {
+        //   const { name, recommendValue, units } = gas.suggestedValues[i];
+        //   const { input }: any = await Enquirer.prompt({
+        //     type: 'input',
+        //     name: 'input',
+        //     message: `Input ${name} (${units})`,
+        //     initial: recommendValue,
+        //   });
+        //   opts.gasArgs[name] = input;
+        // }
+        // gasPriceStep = opts;
+      } else {
+        gasPriceStep = { gasMode: gasMode.name };
+      }
+
+      const transaction = await patch(`/v1/transaction/${partialTx.id}`, {
+        gasPriceStep,
+      });
+
+      const signed = await wallet.signTransaction(
+        JSON.parse(transaction.unsignedTransaction)
+      );
+
+      const result = await post(`/v1/transaction/${partialTx.id}/submit`, {
+        signedTransaction: signed,
+      });
+      lastTx = { network: transaction.network, result: result };
+
+      while (true) {
+        const result = await get(
+          `/v1/transaction/status/${lastTx.network}/${lastTx.result.transactionHash}`
+        );
+        console.log(result.status);
+        if (result.status === "success") {
+          console.log(lastTx.link);
+          break;
+        } else if(result.status === "failed") {
+          console.log('TRANSACTION FAILED');
+          break;
+        } else {
+          console.log("Pending...");
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+      }
+    }
+  } catch (error) {
+    if (error) {
+      console.log('Script failed');
+      console.log(error);
     } else {
-      console.log("Pending...");
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log('Script was aborted.')
     }
   }
 }
+
 main();
